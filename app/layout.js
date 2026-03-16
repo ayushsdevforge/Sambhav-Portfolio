858960b3bf4579b8766f36a19f96307a8466ffbd
sambhavmehra <152503158+sambhavmehra@users.noreply.github.com>
2026-03-17 01:24:08 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -182,7 +182,7 @@ return ( <html lang="en"> <head>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-XXXXXXXXXX');
+          gtag('config', 'G-N11LJW7FL');
         `,
       }}
     />
